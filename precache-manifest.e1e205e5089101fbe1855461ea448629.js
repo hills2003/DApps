e8d97d23716527652c3314567661ffa2395a22b9
Hillary victor <chillaryvictor@gmail.com>
2022-04-03 15:45:02 -0700

    Updates

---
+++ precache-manifest.e1e205e5089101fbe1855461ea448629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987b2e8a30dd1c6ca220b650695ab753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b6ba35c1b8776f8b8ba",
+    "url": "/static/css/2.37c457eb.chunk.css"
+  },
+  {
+    "revision": "66c4f9799bc225a867e3",
+    "url": "/static/css/main.6bc64add.chunk.css"
+  },
+  {
+    "revision": "9b6ba35c1b8776f8b8ba",
+    "url": "/static/js/2.e8e76c30.chunk.js"
+  },
+  {
+    "revision": "66c4f9799bc225a867e3",
+    "url": "/static/js/main.08a35c33.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
